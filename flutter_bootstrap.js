5219703c04ed6ff5edffb22edfe2a0fd8e7b4a6b
PC-ORBIT <PC-ORBIT@DESKTOP-IK6P0R2>
2026-05-11 02:55:35 +0300

    Update: fixed layout and added new features

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2105452177" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "314500029" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
